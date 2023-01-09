5d38cd73e2880d4b8331928e3d852a5682615f4b
Emeka Nwoji <ednwoji@unionbankng.com>
2023-01-09 19:15:23 +0100

    Added Files for copying Footages

--- src/main/java/com/ubnarchival/archival/ArchivalApplication.java
+++ src/main/java/com/ubnarchival/archival/ArchivalApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
+import java.io.File;
+
 @SpringBootApplication
 public class ArchivalApplication {
 
@@ -11,6 +13,21 @@ public class ArchivalApplication {
 		SpringApplication.run(ArchivalApplication.class, args);
 //		String password = BCrypt.hashpw("test1234", BCrypt.gensalt());
 //		System.out.println(password);
+
+
+		File dir = new File("C:\\Users\\ednwoji\\Documents");
+
+		// Get the list of files in the directory
+		File[] files = dir.listFiles();
+
+		// Iterate through the files and find the folder that starts with the given name
+		for (File file : files) {
+			if (file.getName().startsWith("archival") || file.getName().endsWith(".zip")) {
+				// This is the folder we're looking for
+				System.out.println(file.getAbsolutePath());
+				break;
+			}
+		}
 
 
 	}
